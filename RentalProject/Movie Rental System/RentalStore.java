05c864b9c9f33cde45763520809cd2efcf30e73f
pashaz7b <amyrhsynpasha2@gmail.com>
2023-05-03 23:16:33 +0430

    project updated

--- RentalProject/Movie Rental System/RentalStore.java
+++ RentalProject/Movie Rental System/RentalStore.java
@@ -1,41 +1,112 @@
 import java.util.*;
 
 public class RentalStore {
-    private List<Movie> availableMovies;
+    private List<Movie> movies;
     private List<Customer> customers;
-    private List<Rental> rentals;
 
     public RentalStore() {
-        availableMovies = new ArrayList<>();
+        movies = new ArrayList<>();
         customers = new ArrayList<>();
-        rentals = new ArrayList<>();
     }
 
     public void register(Customer customer) {
         customers.add(customer);
+        System.out.println("YOU HAVE BEEN REGISTERED SUCCESSFULLY!");   
     }
 
     public void addMovie(Movie movie) {
-        availableMovies.add(movie);
+        movies.add(movie);
+        System.out.println("THE MOVIE HAS BEEN ADDED SUCCESSFULLY!");
     }
 
     public void removeMovie(Movie movie) {
-        availableMovies.remove(movie);
+        boolean found = false;
+        for (Movie m : movies)
+        {
+            if(m.getId()== movie.getId() && m.isAvailable())
+            {
+                movies.remove(movie);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found)
+        {
+            System.out.println("COULDN'T FIND THE MOVIE! ");
+        }
+        
     }
 
     public List<Movie> getAvailableMovies() {
-        return availableMovies;
+
+        List<Movie> availableMovieList = new ArrayList<>();
+
+        for (Movie m : movies)
+        {
+            if(m.isAvailable())
+            {
+                availableMovieList.add(m);
+            }
+        }
+
+        return availableMovieList;
     }
 
     public void rentMovie(Movie movie, Customer customer) {
-        Rental rental = new Rental(movie, customer, rentals.size() + 1);
-        rentals.add(rental);
-        movie.setAvailable(false);
+
+        boolean existed = false;
+        boolean rented = false;
+        
+        for (Movie m : movies)
+        {
+            if(m.getId()==movie.getId())
+            {
+                existed = true;
+
+                if(m.isAvailable()==true)
+                {
+                    String str1 = Integer.toString(movie.getId()) + Integer.toString(customer.getId());
+                    int id = Integer.parseInt(str1);
+                    Rental rental = new Rental(movie, customer, id);
+                    customer.getRentals().add(rental);
+                    movie.setAvailable(false);
+                    rented = true;
+                    System.out.println("YOU HAVE SUCCESSFULLY RENTED THIS MOVIE!");
+                }
+
+            }
+        }
+
+        if (!existed)
+        {
+            System.out.println("SORRY THERE IS NO MOVIE WITH THIS INFORMATION");
+        }
+        if(existed && !rented )
+        {
+            System.out.println("SORRY THI MOVIE HAS BEEN RENTED");
+        }
+
+
     }
 
     public void returnMovie(Rental rental) {
-        rental.getMovie().setAvailable(true);
-        rental.setReturnDate(new Date());
+
+        int i=0;
+        for(Customer c : customers)
+        {
+            if (c.getRentals().get(i).getId()==rental.getId())
+            {
+                rental.getMovie().setAvailable(true);
+                rental.setReturnDate(new Date());
+                c.getRentals().remove(i);
+                System.out.println("YOU HAVA SUCCESSFULLY RETURNED THIS MOVIE!");
+                break;
+            }
+            
+            i++;
+        }
+        
     }
 
     public Customer getCustomerById(int id) {
@@ -48,7 +119,7 @@ public class RentalStore {
     }
 
     public Movie getMovieById(int id) {
-        for (Movie movie : availableMovies) {
+        for (Movie movie : movies) {
             if (movie.getId() == id) {
                 return movie;
             }
